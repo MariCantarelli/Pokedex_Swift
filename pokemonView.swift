72f45c99eeb76f2ea2566805b39ad9754c1fd541
m2ri16 <00000845849.marina@unicap.br>
2025-03-24 16:07:13 -0300

    pokemon view

--- pokemonView.swift
+++ pokemonView.swift
@@ -4,9 +4,16 @@ struct pokemonView: View {
     
     @State var pokemon: Pokemon
     var body: some View {
-        VStack {
-            Image(pokemon.pokemonImage)
-            Text(pokemon.name)
+        ScrollView(.vertical, showsIndicators: false){
+            VStack {
+                Image(pokemon.defImage)
+                    .resizable()
+                    .frame(width: 100, height: 100) //ajustar tamanho das estatiticas
+                Text(pokemon.name)
+                Image(pokemon.pokemonImage)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+            }
         }
     }
 }
