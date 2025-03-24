86cf60f59ac891034ebc6d2b30189dc833ebcf14
m2ri16 <00000845849.marina@unicap.br>
2025-03-24 16:04:50 -0300

    lista sete

--- listaseta.swift
+++ listaseta.swift
@@ -1,28 +1,21 @@
 import SwiftUI
 
-struct ListaDoPoke: View {
+struct listaoficial: View {
     let pokemons: [Pokemon]
     
     var body: some View {
-        NavigationView{
+        NavigationView {
             List(pokemons) { pokemon in
-                NavigationLink(destination: pokemonView (pokemon: pokemon)) {
-                        Text(pokemon.name.capitalized) // Exibe o nome do Pokémon com a primeira letra maiúscula
-                            .font(.headline)
-                        HStack {
-                            Text("Types: \(pokemon.types.map { $0.rawValue }.joined(separator: ","))") // Exibe os tipos
-                                .font(.subheadline)
-                        }
+                NavigationLink(destination: pokemonView(pokemon: pokemon)) {
+                    Text(pokemon.name.capitalized) // Exibe o nome do Pokémon com a primeira letra maiúscula
+                        .font(.headline)
+                    HStack {
+                        Text("Types: \(pokemon.types.map { $0.rawValue }.joined(separator: ","))") // Exibe os tipos
+                            .font(.subheadline)
                     }
-
+                }
             }
         }
     }
 }
-
-//struct ListaDoPoke_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ListaDoPoke()
-//    }
-//}
 
