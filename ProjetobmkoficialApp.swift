79a7876d83ee38f009c6afd5eafc468e645d9124
m2ri16 <00000845849.marina@unicap.br>
2025-03-24 16:02:59 -0300

    proj

---
+++ ProjetobmkoficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetobmkoficialApp.swift
+//  Projetobmkoficial
+//
+//  Created by Aluno Mack on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjetobmkoficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
